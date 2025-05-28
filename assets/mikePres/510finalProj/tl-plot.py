77dc0a2f1499ffacd53accf92eb9d07ecc83e450
Michael Raba <michael.raba@gmail.com>
2025-05-28 04:11:07 -0400

    add insertion loss from sidlab

--- assets/mikePres/510finalProj/tl-plot.py
+++ assets/mikePres/510finalProj/tl-plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load CSV data
-df = pd.read_csv('./imag/510-muffler-TL-20degC.csv')
+df = pd.read_csv("./imag/510-muffler-TL-20degC.csv")
 
 # Extract frequency and TL columns
 frequencies = df.iloc[:, 0]
@@ -12,11 +12,12 @@ transmission_loss = df.iloc[:, 1]
 
 # Plot
 plt.figure(figsize=(10, 6))
-plt.plot(frequencies, transmission_loss, color='darkblue', linewidth=2)
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Transmission Loss (dB) at 20°C')
-plt.title('Muffler Transmission Loss vs Frequency at 20°C')
+plt.plot(frequencies, transmission_loss, color="darkblue", linewidth=2)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Transmission Loss (dB) at 20°C")
+plt.title("Muffler Transmission Loss vs Frequency at 20°C")
 plt.grid(True)
 plt.tight_layout()
-#plt.show()
-plt.savefig('muffler_TL_20degC.png', dpi=300)
+# plt.show()
+# plt.savefig('muffler_TL_20degC.png', dpi=300)
+plt.savefig("muffler_TL_20degC.svg")
