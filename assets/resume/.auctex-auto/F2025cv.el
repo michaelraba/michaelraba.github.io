3cd1f589cd9d51fca0786386855f301838ff2e37
Michael Raba <michael.raba@gmail.com>
2025-09-15 21:26:00 -0400

    jobFairBook, resume

---
+++ assets/resume/.auctex-auto/F2025cv.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "F2025cv"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "mcdowellcv"
+    "mcdowellcv10"
+    "amsmath")
+   (LaTeX-add-environments
+    '("cvsubsection" LaTeX-env-args ["argument"] 3)))
+ :latex)
+
